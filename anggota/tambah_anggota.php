1a385ab275680151babb887cc5d83bb220b3dbaf
rezafikkri <fikkri.reza@gmail.com>
2020-02-17 14:37:58 +0700

    memperbaiki design halaman tambah_anggota

--- anggota/tambah_anggota.php
+++ anggota/tambah_anggota.php
@@ -1,11 +1,11 @@
 <div class="col-lg-6 col-lg-offset-3">
+	<h2 class="judul text-center">Tambah Anggota</h2>
 	<form>
-	<div class="panel panel-default mt-30">
-		<div class="panel-heading"><h2 class="panel-title">Tambah Anggota</h2></div>
+	<div class="panel panel-success mt-30">
 		<div class="panel-body">
 		    <label>Nama Anggota</label>
 		    <p class="text-danger">Nama tidak boleh kosong</p>
-		    <input type="text" name="nama" class="form-control mb-10" placeholder="Nama ...">
+		    <input type="text" name="nama" class="form-control mb-10" placeholder="Nama Lengkap ...">
 		</div>
 	</div>
 	<a href="<?= config::base_url(''); ?>" class="btn btn-default">Kembali!</a>
